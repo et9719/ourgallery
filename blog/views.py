68a173721fc5c8fbc77aa0a196182c9e04b2a81f
et9719 <ella-t@live.co.uk>
2022-03-22 21:26:35 +0000

    Add base.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    ''' What the users should be able to see on the site '''
+    # What model we are using for this view
+    model = Post
+    # Only show posts that have a status of published and order
+    # them by the date they were created on.
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    # What page users will see this on
+    template_name = 'index.html'
+    # how many posts we want to see on the page at one time.
+    paginate_by = 6
